40104a64414837a5918bcf6fe46c8159780200b3
Tomasz Grzesiak <grzesiaktomasz@gmail.com>
2025-01-10 18:13:24 +0100

    Section 1 of 5. Getting started.

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector("#open-nav-menu").addEventListener("click", function() {
+    document.querySelector("header nav .wrapper").classList.add("nav-open");
+} );
+
+document.querySelector("#close-nav-menu").addEventListener("click", function() {
+    document.querySelector("header nav .wrapper").classList.remove("nav-open");
+} );
+
+let customer = "Sara";
+console.log("Hello, "+customer);
